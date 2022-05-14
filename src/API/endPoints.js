e299e11d8efa2013f0522231bc5c76aa7d58d841
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-14 18:29:16 +0300

    Update end points

--- src/API/endPoints.js
+++ src/API/endPoints.js
@@ -3,3 +3,5 @@ export const getNearByCity = (latitude, longitude) => {
   longitude = longitude > 0 ? `%2B${longitude}` : longitude;
   return `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?location=${latitude}${longitude}&limit=10`;
 }
+export const cityWeatherEndPoint = (cityLatitude, cityLongitude) =>
+  `https://api.openweathermap.org/data/2.5/weather?lat=${cityLatitude}&lon=${cityLongitude}&appid=6902c072c502ba82c29e7bb1ee6cb7ec`
