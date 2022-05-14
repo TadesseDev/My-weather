cefaba5a634c7fcce01d91f893aff3125d7e62c0
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-14 19:01:09 +0300

    Configur redux logger

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from 'redux-thunk'
+import logger from 'redux-logger'
 import appReducer from './App/App'
 const rootReducer = combineReducers({
   cities: appReducer,
 })
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 export default store;
