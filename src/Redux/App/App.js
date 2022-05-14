8b9a9711993738541d56c3bf74c0a0dffe296f75
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-14 18:29:42 +0300

    Creat dispatch action for intial update City

--- src/Redux/App/App.js
+++ src/Redux/App/App.js
@@ -1,20 +1,32 @@
 import fetchCitiesData from '../../API/geodb-cities';
-const UPDATE_INITIAL_CITIES = 'APP/initialize_app_with_city';
+import cityWeather from '../../API/currentWeatherData'
+const UPDATE_CITIES = 'APP/initialize_app_with_city';
 export const updateInitialData = () => async (dispatch) => {
-  let data = [];
+  const routeCityData = async (latitude, longitude) => {
+    let cities = [];
+    if (latitude && longitude) {
+      cities = await fetchCitiesData(latitude, longitude);
+    }
+    else cities = await fetchCitiesData();
+    cities = cities.data;
+    for (let i = 0; i < cities.length; i++) {
+      const cityInfo = await cityWeather(cities[i].latitude, cities[i].longitude)
+      const { name, clouds, coord, main, sys, wind } = cityInfo;
+      dispatch({ type: UPDATE_CITIES, payload: [{ name, clouds, coord, main, sys, wind }] });
+    }
+  }
   navigator.geolocation.getCurrentPosition(async ({ coords }) => {
-    data = await fetchCitiesData(coords.latitude, coords.longitude);
-    console.log("data is", data);
+    routeCityData(coords.latitude, coords.longitude);
   }, async denied => {
-    data = await fetchCitiesData();
+    routeCityData();
   });
 }
 
 export default function appReducer(state = [], action) {
   console.log(action);
   switch (action) {
-    case UPDATE_INITIAL_CITIES:
-      return action.payload;
+    case UPDATE_CITIES:
+      return [...state, ...action.payload];
     default:
       return state;
   }
