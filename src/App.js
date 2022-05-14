f5bec3811b43751f8af4c4ff587ad9a3f1941d68
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-14 13:35:24 +0300

    Fetch first 10 cities data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-
+import Home from './pages/Home';
+import React from 'react';
 function App() {
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+      'X-RapidAPI-Key': '566c430e66mshb3ce2e6895aae75p17f4e4jsnc091a7b69f86'
+    }
+  };
+
+  fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/cities?location=%2B11.3494247%2B37.9784585&limit=10', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
   return (
     <div className="App">
+      <Home />
       app component
     </div>
   );
