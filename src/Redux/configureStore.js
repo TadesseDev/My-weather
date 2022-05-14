8eea6774cc6814214f5040f9c6ef90fd14df0184
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-14 18:46:16 +0300

    Configure thunk and middleware

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
+import thunk from 'redux-thunk'
 import appReducer from './App/App'
 const rootReducer = combineReducers({
   cities: appReducer,
 })
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 export default store;
