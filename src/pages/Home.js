c1ce6d1d120b3941e14c5de6df35c36f9d74ebd5
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-14 19:09:24 +0300

    Remove unused comment and console statement

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,7 @@ import CityWeather from '../components/CityWeather';
 export default function Home() {
 
   const cities = useSelector(store => store.cities);
-  console.log("state is", cities);
   return (
-
     <div>{cities.length}</div>
   )
 }
