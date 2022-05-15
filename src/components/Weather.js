5fc64fdb3faf1c340d79a96822a4ed7ccd36d110
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-15 11:03:47 +0300

    Update city card rout

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import { Card, Col } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 export default function Weather({ city }) {
+  const navigate = useNavigate();
   return (
-    <Col xs={6} sm={4} lg={2} className="p-0">
-      <NavLink to={`/detail?id=${city.id}`}>
-        <Card w={100} >
-          <Card.Body>
-            <Card.Title>
-              {city.name}
-            </Card.Title>
-            <Card.Text>city detail</Card.Text>
-          </Card.Body>
-        </Card>
-      </NavLink>
+    <Col xs={6} sm={4} lg={2} className="p-0" onClick={() => navigate(`/detail?id=${city.id}`)}>
+      <Card w={100} >
+        <Card.Body>
+          <Card.Title>
+            {city.name}
+          </Card.Title>
+          <Card.Text>city detail</Card.Text>
+        </Card.Body>
+      </Card>
     </Col >
   )
 }
