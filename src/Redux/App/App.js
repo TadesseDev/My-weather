80dc4a0f71abf1f6c173e99258f9345f67342185
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-15 14:00:57 +0300

    Update Redux/App to load other cities

--- src/Redux/App/App.js
+++ src/Redux/App/App.js
@@ -1,21 +1,29 @@
 import fetchCitiesData from '../../API/geodb-cities';
 import cityWeather from '../../API/currentWeatherData'
 import { v4 } from 'uuid';
+import MAJOR_CITIES from '../../GLOBAL/MAJOR_CITIES'
 const UPDATE_CITIES = 'APP/initialize_app_with_city';
+
+const updateStoreWithCityData = (cities, nearMe, dispatch) => {
+  cities.forEach(async city => {
+    const cityInfo = await cityWeather(city.latitude, city.longitude)
+    const { name, distance = false, population } = city;
+    const { clouds, coord, main, sys, wind, weather, unit } = cityInfo;
+    dispatch({
+      type: UPDATE_CITIES,
+      payload: [{ id: v4(), name, distance, population, nearMe, clouds, coord, weather, unit, main, sys, wind }]
+    });
+  });
+}
 
 const routeCityData = async (dispatch, latitude, longitude) => {
   let cities = [];
   if (latitude && longitude) {
     cities = await fetchCitiesData(latitude, longitude);
+    cities = cities.data;
+    updateStoreWithCityData(cities, true, dispatch);
   }
-  else cities = await fetchCitiesData();
-  cities = cities.data;
-  cities.forEach(async city => {
-    const cityInfo = await cityWeather(city.latitude, city.longitude)
-    const { name, distance = false, population } = city;
-    const { clouds, coord, main, sys, wind } = cityInfo;
-    dispatch({ type: UPDATE_CITIES, payload: [{ id: v4(), name, distance, population, clouds, coord, main, sys, wind }] });
-  });
+  updateStoreWithCityData(MAJOR_CITIES, false, dispatch);
 }
 
 export const updateInitialData = () => async (dispatch) => {
