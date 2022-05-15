f331382bbb2f773f8aa6bf0ae5fecb5f3f602bd7
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-15 20:29:16 +0300

    Remove comment and console statement

--- src/API/currentWeatherData.js
+++ src/API/currentWeatherData.js
@@ -3,10 +3,8 @@ import { cityWeatherEndPoint } from '../GLOBAL/endPoints';
 // we chan use mesure to change temp
 const cityWeather = async (cityLatitude, cityLongitude, unit = "metric") => {
   const URL = cityWeatherEndPoint(cityLatitude, cityLongitude, unit);
-  console.log(URL);
   let response = await fetch(URL);
   response = await response.json();
-  console.log(response);
   unit = unit === "metric" ? "℃" : unit === "imperial" ? 'F' : "K";
   return {
     ...response, unit
