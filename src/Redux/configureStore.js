cf2c1b4a680681fcf9576103d5d9d4ca48968020
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-15 22:49:11 +0300

    Apply multiple reducer in combine reducer

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,9 +1,11 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
-import appReducer from './app/app'
+import appReducer from './app/app';
+import searchReducer from './components/navigation/navigation'
 const rootReducer = combineReducers({
   cities: appReducer,
+  cities: searchReducer,
 })
 
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
