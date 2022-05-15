135c90d559e370434f2fed8bb990b2ad493bcfc4
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-15 22:48:21 +0300

    Create and apply reducer for earch option

--- src/Redux/components/navigation/navigation.js
+++ src/Redux/components/navigation/navigation.js
@@ -1,8 +1,32 @@
 import getCityDetail from '../../../API/name-to-coordinate'
+import cityWeather from '../../../API/currentWeatherData'
+import { v4 } from 'uuid';
 const ADD_SINGLE_CITY = "Redux/components/navigation/navigation/ADD_SINGLE_CITY";
 
 export const searchAndAddCity = (cityName, navigator) => async (dispatch) => {
-  const data = await getCityDetail(cityName);
-  console.log(data);
-  navigator("/detail?'this is id");
+  let city = await getCityDetail(cityName);
+  city = city[0];
+  const cityInfo = await cityWeather(city.lat, city.lon);
+  const { name, distance = false, population = 0, country, country: countryCode } = city;
+  const { clouds, coord, main, wind, weather, unit } = cityInfo;
+  console.log(city);
+  const action = {
+    type: ADD_SINGLE_CITY,
+    payload: { id: v4(), name, distance, population, nearMe: false, country, countryCode, clouds, coord, weather, unit, main, wind }
+  }
+  dispatch(action);
+  console.log(action);
+  navigator(`/detail?id=${action.payload.id}`);
 }
+
+const searchReducer = (state = [], action) => {
+  console.log("dispatching action from search");
+  switch (action.type) {
+    case ADD_SINGLE_CITY:
+      return [action.payload, ...state];
+    default:
+      return state;
+  }
+}
+
+export default searchReducer;
