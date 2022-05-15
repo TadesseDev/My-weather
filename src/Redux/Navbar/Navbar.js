be5abd0ecd308a46836fac8a85179913f88961c6
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-15 22:09:21 +0300

    Rename navigation component

--- src/Redux/Navbar/Navbar.js
+++
@@ -1,1 +0,0 @@
-const ADD_SINGLE_CITY = "Redux/Navbar/Navbar/ADD_SINGLE_CITY";
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { Navbar, Container, Form } from 'react-bootstrap';
 import { CgSearchLoading } from 'react-icons/cg';
+import { searchAndAddCity } from '../Redux/components/navigation/navigation'
+import { useNavigate } from 'react-router-dom';
 export default function Navigation() {
   const [cityToSearch, updateCityToSearch] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const updateSearchValue = (event) => {
     updateCityToSearch(() => event.target.value)
-    console.log(cityToSearch);
   }
   const searchCity = () => {
     console.log(cityToSearch);
+    dispatch(searchAndAddCity(cityToSearch, navigate));
   }
   return (
     <Navbar bg="light" expand="sm">
