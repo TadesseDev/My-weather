ce3b2ad7f34a7bf64fee115e6852199358c42f5e
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-15 16:10:20 +0300

    distplay city detail in detail component

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSearchParams } from 'react-router-dom'
 import { useSelector } from 'react-redux';
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 export default function Detail() {
   const [getParam, setParam] = useSearchParams();
   let city = { id: getParam.get('id') }
@@ -13,7 +13,21 @@ export default function Detail() {
   });
   return (
     city && <Container>
-
+      <Row>
+        <h1>{city.name}</h1>
+        <h4>{city.weather[0].description}</h4>
+        <small>max-tmp: {city.main.temp_max}</small>
+        <small>min-temp{city.main.temp_min}</small>
+      </Row>
+      <Row ><Col>Cloud</Col> <Col >{city.clouds.all}%</Col></Row>
+      <Row><Col>Wind</Col> <Col >
+        speed: {city.wind.speed} |
+        deg: {city.wind.deg}° |
+        gust: {city.wind.gust} </Col></Row>
+      <Row ><Col>Humidity</Col> <Col >{city.main.humidity}%</Col></Row>
+      <Row ><Col>Sea level</Col> <Col >{city.main.sea_level}%</Col></Row>
+      <Row ><Col>Population</Col> <Col >{city.population || 'Not available'}</Col></Row>
+      <Row><Col>Approximate distance</Col> <Col >{city.distance && city.distance + "KM" || 'Not available'}</Col></Row>
     </Container> || <div>No city found</div>
   )
 }
