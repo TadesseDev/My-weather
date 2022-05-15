700d9f004a3928c3a731f0ae9ac5a54458d82038
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-15 11:21:31 +0300

    Get all city detail in Weather detail

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import { useSearchParams } from 'react-router-dom'
+import { useSelector } from 'react-redux';
 export default function Detail() {
   const [getParam, setParam] = useSearchParams();
-  console.log(getParam.get('id'));
+  let city = { id: getParam.get('id') }
+  city = useSelector(store => {
+    for (let thisCity of store.cities) {
+      if (thisCity.id === city.id)
+        return thisCity;
+    }
+  });
   return (
     <div>Detail</div>
   )
