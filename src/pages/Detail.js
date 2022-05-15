f6bce4a9d5549aac8e026803b92f4e8c6e0febc8
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-15 11:13:46 +0300

    Add a useParam from the detail page

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,6 +1,8 @@
 import React from 'react'
-
+import { useSearchParams } from 'react-router-dom'
 export default function Detail() {
+  const [getParam, setParam] = useSearchParams();
+  console.log(getParam.get('id'));
   return (
     <div>Detail</div>
   )
