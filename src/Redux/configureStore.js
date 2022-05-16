3e0a0909e6a0f89e9d824938a0a163a24eee6612
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-16 13:54:21 +0300

    Implement applFilter action successfully

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -2,8 +2,10 @@ import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 import appReducer from './app/app';
+import { filterReducer } from './components/navigation/navigation'
 const rootReducer = combineReducers({
   cities: appReducer,
+  filter: filterReducer,
 })
 
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
