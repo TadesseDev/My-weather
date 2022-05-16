5778b53e2e1f8efe420722bfe1f487d402078ccb
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-17 00:13:33 +0300

    Add and style hr tag

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,15 +15,17 @@ export default function Home() {
     (filterType === 'All') && < Container >
       <h3 className="title">My cities</h3>
       <Row >
-        {myCity.length > 0 && myCity.map(city => <Weather key={city.id} city={city} />) || <p>You havent checked any city yet</p>}
+        {myCity.length > 0 && myCity.map(city => <Weather key={city.id} city={city} />) || <p className="warning">You havent checked any city yet</p>}
       </Row>
+      <hr />
       <h3 className="title">Cities near you</h3>
       <Row >
-        {cityNearMe.length > 0 && cityNearMe.map(city => <Weather key={city.id} city={city} />) || <p>We couldn't find your location</p>}
+        {cityNearMe.length > 0 && cityNearMe.map(city => <Weather key={city.id} city={city} />) || <p className="warning">We couldn't find your location</p>}
       </Row>
+      <hr />
       <h3 className="title">Other cities</h3>
       <Row >
-        {otherCities.length > 0 && otherCities.map(city => <Weather key={city.id} city={city} />) || <p>You haven't checked other cities</p>}
+        {otherCities.length > 0 && otherCities.map(city => <Weather key={city.id} city={city} />) || <p className="warning">You haven't checked other cities</p>}
       </Row>
     </ Container> || <FilteredCities filter={filterType} cities={cities} />
   )
