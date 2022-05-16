9f76494acbfb7fa1685909ddc0c856e05b041690
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-16 14:54:50 +0300

    Add error handling for name-to-city API

--- src/API/name-to-coordinate.js
+++ src/API/name-to-coordinate.js
@@ -1,8 +1,12 @@
 import { searchCityEndPoint } from '../GLOBAL/endPoints';
 const getCityDetail = async (cityName) => {
-  const URL = searchCityEndPoint(cityName);
-  let response = await fetch(URL);
-  response = await response.json();
-  return response;
+  try {
+    const URL = searchCityEndPoint(cityName);
+    let response = await fetch(URL);
+    response = await response.json();
+    return response;
+  } catch (error) {
+    throw Error("Fail to fetch city detail", error);
+  }
 }
 export default getCityDetail;
