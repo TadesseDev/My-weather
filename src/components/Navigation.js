c725a6212438fe4dc983ce874ef3ab1b65d74ad1
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-16 13:52:48 +0300

    replace category form insto controled field

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Navbar, Container, Form } from 'react-bootstrap';
 import { CgSearchLoading } from 'react-icons/cg';
-import { searchAndAddCity } from '../Redux/components/navigation/navigation'
+import { searchAndAddCity, applyFilter, removeFilter } from '../Redux/components/navigation/navigation'
 import { useNavigate } from 'react-router-dom';
 export default function Navigation() {
   const [cityToSearch, updateCityToSearch] = useState("");
@@ -15,18 +15,21 @@ export default function Navigation() {
     console.log(cityToSearch);
     dispatch(searchAndAddCity(cityToSearch, navigate));
   }
+  const filter = (event) => {
+    dispatch(applyFilter(event.target.value));
+  }
   return (
     <div id="navigation">
       <Container>
-        <form id="filter">
-          <label for="category">Category</label>
-          <select name="category" id="category">
+        <div id="filter">
+          <p>filter with category</p>
+          <select name="category" id="category" onChange={filter}>
             <option value="All">All</option>
             <option value="My cities">My cities</option>
             <option value="Near by cities">Near by cities</option>
             <option value="Major cities">Major cities</option>
           </select>
-        </form>
+        </div>
         <div id="search">
           <input type="text"
             placeholder="Search your city"
