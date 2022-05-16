99bf7c9d5e3ffb98ffd7a8eeca98cad1e7a6e2f0
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-16 23:45:44 +0300

    Add weather map and render it to dom

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import { Card, Col } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom'
+import { BsFillCloudDrizzleFill, BsFillCloudFog2Fill, BsFillSunFill } from 'react-icons/bs';
+import { GiRaining } from 'react-icons/gi'
+import { IoIosPartlySunny } from 'react-icons/io'
 export default function Weather({ city }) {
   const navigate = useNavigate();
+  const weatherIcons = new Map();
+  weatherIcons.set("light rain", <GiRaining className="weather-icon" />);
+  weatherIcons.set("overcast clouds", <BsFillCloudDrizzleFill className="weather-icon" />);
+  weatherIcons.set("few clouds", <IoIosPartlySunny className="weather-icon" />);
+  weatherIcons.set("broken clouds", <BsFillCloudFog2Fill className="weather-icon" />);
+  weatherIcons.set("clear sky", <BsFillSunFill className="weather-icon" />);
   return (
     <Col xs={6} sm={4} lg={2} className="p-0" onClick={() => navigate(`/detail?id=${city.id}`)}>
       <Card className="w-100 h-100">
@@ -14,6 +23,7 @@ export default function Weather({ city }) {
         </Card.Body>
         <small>{city.main.temp}{city.unit}</small>
         <small>{city.country}</small>
+        {weatherIcons.get(city.weather[0].description)}
       </Card>
     </Col >
   )
