2ddef418465d804547aa7525d32961ef7dd2707f
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-16 15:59:28 +0300

    Add Home button

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { useSearchParams } from 'react-router-dom'
+import { useSearchParams, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux';
 import { Container, Row, Col } from 'react-bootstrap'
 export default function Detail() {
   const [getParam, setParam] = useSearchParams();
+  const navigate = useNavigate();
   let city = { id: getParam.get('id') }
   city = useSelector(store => {
     for (let thisCity of store.cities) {
@@ -12,23 +13,26 @@ export default function Detail() {
     }
   });
   return (
-    city && <Container>
-      <Row>
-        <h1>{city.name}</h1>
-        <h4>{city.weather[0].description}</h4>
-        <small>max-tmp: {city.main.temp_max}</small>
-        <small>min-temp{city.main.temp_min}</small>
-      </Row>
-      <Row ><Col>Cloud</Col> <Col >{city.clouds.all}%</Col></Row>
-      <Row><Col>Wind</Col> <Col >
-        speed: {city.wind.speed} |
-        deg: {city.wind.deg}° |
-        gust: {city.wind.gust} </Col></Row>
-      <Row ><Col>Humidity</Col> <Col >{city.main.humidity}%</Col></Row>
-      <Row ><Col>Sea level</Col> <Col >{city.main.sea_level || "Not available"}</Col></Row>
-      <Row ><Col>Population</Col> <Col >{city.population || 'Not available'}</Col></Row>
-      <Row><Col>Approximate distance</Col> <Col >{city.distance && city.distance + "KM" || 'Not available'}</Col></Row>
-      <Row><Col>Coordinate</Col> <Col><div>Lat: {city.coord.lat}</div><div>Lat: {city.coord.lat}</div></Col></Row>
-    </Container> || <div>No city found</div>
+    <div>
+      {city && <Container>
+        <Row>
+          <h1>{city.name}</h1>
+          <h4>{city.weather[0].description}</h4>
+          <small>max-tmp: {city.main.temp_max}</small>
+          <small>min-temp{city.main.temp_min}</small>
+        </Row>
+        <Row ><Col>Cloud</Col> <Col >{city.clouds.all}%</Col></Row>
+        <Row><Col>Wind</Col> <Col >
+          speed: {city.wind.speed} |
+          deg: {city.wind.deg}° |
+          gust: {city.wind.gust} </Col></Row>
+        <Row ><Col>Humidity</Col> <Col >{city.main.humidity}%</Col></Row>
+        <Row ><Col>Sea level</Col> <Col >{city.main.sea_level || "Not available"}</Col></Row>
+        <Row ><Col>Population</Col> <Col >{city.population || 'Not available'}</Col></Row>
+        <Row><Col>Approximate distance</Col> <Col >{city.distance && city.distance + "KM" || 'Not available'}</Col></Row>
+        <Row><Col>Coordinate</Col> <Col><div>Lat: {city.coord.lat}</div><div>Lat: {city.coord.lat}</div></Col></Row>
+      </Container> || <div>No city found</div>}
+      <button onClick={() => navigate('/')}>Home</button>
+    </div>
   )
 }
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams, useNavigate } from 'react-router-dom';
 export default function Error() {
   const [getParam, setParam] = useSearchParams();
   const errorMessage = getParam.get('message');
+  const navigate = useNavigate();
   return (
     <div id="error">
       <h1>Error</h1>
       <h3>{errorMessage}</h3>
+      <button onClick={() => navigate('/')}>Home</button>
     </div>
   )
 }
