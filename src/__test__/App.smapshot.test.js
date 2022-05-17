4a7d1cf867991c8eed3dc0ab8f073895b07def53
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-17 20:22:38 +0300

    Update snapshot test with AppMock

--- src/__test__/App.smapshot.test.js
+++ src/__test__/App.smapshot.test.js
@@ -1,7 +1,5 @@
 import renderer from 'react-test-renderer';
-import App from '../App';
-import { Provider } from 'react-redux'
-import store from '../Redux/configureStore'
+import MockAppComponent from '../__mocks__/RenderAppWithProvider'
 import * as appRedux from '../Redux/App/App'
 jest.spyOn(appRedux, 'updateInitialData')
   .mockImplementation(() => (dispatch) => {
@@ -10,9 +8,7 @@ jest.spyOn(appRedux, 'updateInitialData')
 describe('compare snapshot for the App', () => {
   test('Assert App matches its snapshot', () => {
     const appSnap = renderer.create(
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <MockAppComponent />
     ).toJSON();
     expect(appSnap).toMatchSnapshot()
   });
