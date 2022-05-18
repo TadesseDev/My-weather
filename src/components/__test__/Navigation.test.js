70495affbb9a5adb32f5be1eb256b48205a7c418
Tadesse Alemayehu <tadalem26@gmail.com>
2022-05-18 21:51:06 +0300

    Use a mockRenderer for the navigation component

--- src/components/__test__/Navigation.test.js
+++ src/components/__test__/Navigation.test.js
@@ -1,18 +1,12 @@
 import renderer from 'react-test-renderer'
 import Navigation from '../Navigation'
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from '../../Redux/configureStore'
+import MockRender from '../__mocks__/MockRender'
 describe('Snapshot the check for the FilteredCities', () => {
 
   test('should match the snapshot', () => {
     const navigationSnap = renderer
       .create(
-        <Provider store={store}>
-          <BrowserRouter>
-            <Navigation />
-          </BrowserRouter>
-        </Provider>
+        MockRender(<Navigation />)
       ).toJSON();
     expect(navigationSnap).toMatchSnapshot();
   })
