c6e15b59081655e5ead0335a322f0e51bfde2b99
Tadesse Alemayehu <tadalem26@gmail.com>
2022-05-18 21:55:47 +0300

    Use a mockRenderer for the Weather component

--- src/components/__test__/Weather.test.js
+++ src/components/__test__/Weather.test.js
@@ -1,20 +1,14 @@
 
 import renderer from 'react-test-renderer'
 import Weather from '../Weather'
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from '../../Redux/configureStore'
 import city from '../../__mocks__/city'
+import MockRender from '../__mocks__/MockRender'
 describe('Snapshot the check for the FilteredCities', () => {
 
   test('should match the snapshot', () => {
     const weatherSnap = renderer
       .create(
-        <Provider store={store}>
-          <BrowserRouter>
-            <Weather city={city} />
-          </BrowserRouter>
-        </Provider>
+        MockRender(<Weather city={city} />)
       ).toJSON();
     expect(weatherSnap).toMatchSnapshot();
   })
