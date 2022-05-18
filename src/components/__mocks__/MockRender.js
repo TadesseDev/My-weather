362e121537c65b5ce77910fa324e784e3a9b8f7b
Tadesse Alemayehu <tadalem26@gmail.com>
2022-05-18 21:49:58 +0300

    Add Mockrenderer for the component tests

---
+++ src/components/__mocks__/MockRender.js
@@ -0,0 +1,12 @@
+import '@testing-library/jest-dom';
+import { Provider } from 'react-redux'
+import store from '../../Redux/configureStore'
+import { BrowserRouter } from 'react-router-dom';
+
+export default (component) => (
+  <Provider store={store}>
+    <BrowserRouter>
+      {component}
+    </BrowserRouter>
+  </Provider>
+);
