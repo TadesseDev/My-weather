508aa0412fc3d262735cd5f59b657c1a27c231e3
Tadesse Alemayehu <tadalem26@gmail.com>
2022-05-18 21:44:52 +0300

    Write snapshot test for the Home page

--- src/pages/__test__/Home.test.js
+++ src/pages/__test__/Home.test.js
@@ -1,15 +1,22 @@
 import { render, screen } from '@testing-library/react'
 import Home from '../Home';
 import '@testing-library/jest-dom';
-import { Provider } from 'react-redux'
-import store from '../../Redux/configureStore'
-import { BrowserRouter } from 'react-router-dom';
 import MockRender from '../__mocks__/MockRender'
+import renderer from 'react-test-renderer'
 describe('Home page test', () => {
   test('Should display the error message', () => {
     render(MockRender(<Home />));
     const headings = screen.getAllByRole(/heading/i);
     expect(headings.length >= 3).toBeTruthy();
   })
-
 });
+
+
+describe('Snapshot check for the Home page', () => {
+
+  test('should match the snapshot', () => {
+    const homePageSnap = renderer
+      .create(MockRender(<Home />)).toJSON();
+    expect(homePageSnap).toMatchSnapshot();
+  })
+})
