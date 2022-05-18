f5ae685502589ee17da70bb5ef6247dbdfa93dd5
Tadesse Alemayehu <tadalem26@gmail.com>
2022-05-18 21:44:10 +0300

    Write snapshot test for the Error page

--- src/pages/__test__/error.test.js
+++ src/pages/__test__/error.test.js
@@ -1,10 +1,9 @@
 import { render, screen } from '@testing-library/react'
 import Error from '../Error';
 import '@testing-library/jest-dom';
-import { Provider } from 'react-redux'
-import store from '../../Redux/configureStore'
-import { BrowserRouter } from 'react-router-dom';
 import MockRender from '../__mocks__/MockRender'
+import renderer from 'react-test-renderer'
+
 describe('error page test', () => {
   test('Should display the error message', () => {
     render(
@@ -14,3 +13,13 @@ describe('error page test', () => {
     expect(error).toBeInTheDocument();
   })
 });
+
+
+describe('Snapshot the check for the Error page', () => {
+
+  test('should match the snapshot', () => {
+    const errorPageSnap = renderer
+      .create(MockRender(<Error />)).toJSON();
+    expect(errorPageSnap).toMatchSnapshot();
+  })
+})
