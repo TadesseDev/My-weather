1b9ef21c2429775aeea35019ac1d07954f49f591
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-25 18:31:29 +0300

    Install babel preprocessor

--- server/server.js
+++ server/server.js
@@ -1,3 +1,7 @@
 import express from "express";
 const app = express();
-app.listen(process.env.PORT, () => { console.log("app is running on port 4000") })
+app.listen(process.env.POR || 4000, () => { console.log("app is running on port 4000") })
+
+app.get('/', (req, res) => {
+  res.send("Hitting the root server");
+});
