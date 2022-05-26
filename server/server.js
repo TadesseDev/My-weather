ce248f237ffcf566baa7e3e284110f1d74dec1fb
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-26 21:52:23 +0300

    Sucess connecting to the local server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 import express from "express";
 import dotenv from 'dotenv'
+import mongoose from 'mongoose';
 dotenv.config()
 const app = express();
 app.listen(process.env.PORT || 5000, () => { console.log("app is running on port 4000") })
 
+function main() {
+  mongoose.connect('mongodb://localhost:27017')
+    .then(success => console.log("connected success"));
+}
+
 app.get('/', (req, res) => {
   res.send("You are hitting the root server");
 });
+
+main();
