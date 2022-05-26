0dd9d8e37bad205aaa99087530587ccc5368454f
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-26 22:18:27 +0300

    Create and save the first book

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.listen(process.env.PORT || 5000, () => { console.log("app is running on port 4000") })
 
 function main() {
-  mongoose.connect('mongodb://localhost:27017')
+  mongoose.connect('mongodb://localhost:27017/BookStore')
     .then(success => console.log("connected success"));
 }
 
@@ -15,3 +15,12 @@ app.get('/', (req, res) => {
 });
 
 main();
+
+const books = mongoose.Schema({
+  name: String
+});
+
+const BooksConst = mongoose.model('myBooks', books);
+
+const SYOT = new BooksConst({ name: "say tou are one of them" });
+SYOT.save();
