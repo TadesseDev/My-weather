88014e8a5b26fb5cff22604b9723b883f7920954
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-27 07:59:35 +0300

    Use destructuring and mongo compass

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 import express from "express";
 import dotenv from 'dotenv'
 import mongoose from 'mongoose';
-dotenv.config()
+const { parsed: { PORT, USER_NAME, PASSWORD } } = dotenv.config();
 const app = express();
-app.listen(process.env.PORT || 5000, () => { console.log("app is running on port 4000") })
+app.listen(PORT || 5000, () => { console.log(`app is running on port ${PORT}`) })
 
 function connect() {
-  mongoose.connect('mongodb://localhost:27017/BookStore')
+  mongoose.connect(`mongodb+srv://${USER_NAME}:${PASSWORD}@cluster0.3lkqrdw.mongodb.net/?retryWrites=true&w=majority`)
     .then(success => console.log("connected success"));
 }
 connect();
